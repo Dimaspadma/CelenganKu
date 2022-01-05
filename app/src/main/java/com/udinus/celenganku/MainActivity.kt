21d0b452ad8e1c5d1e497f178b76b62b2423659a
Dimaspadma <dimaspadma@gmail.com>
2022-01-05 23:10:17 +0700

    Migrate to MAD (Modern Android Development) with Fragments

--- app/src/main/java/com/udinus/celenganku/MainActivity.kt
+++ app/src/main/java/com/udinus/celenganku/MainActivity.kt
@@ -4,16 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.udinus.celenganku.databinding.ActivityMainBinding
 
 
 // Splash screen
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         Log.d(TAG, "created")
 
