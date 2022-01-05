069c27734d56427bb7448c837a2c54caec50a1f1
Dimaspadma <dimaspadma@gmail.com>
2022-01-05 23:10:40 +0700

    Add Splash Screen Fragment

--- app/src/main/java/com/udinus/celenganku/SplashScreenFragment.kt
+++ app/src/main/java/com/udinus/celenganku/SplashScreenFragment.kt
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.udinus.celenganku.databinding.FragmentSplashScreenBinding
 
 class SplashScreenFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
